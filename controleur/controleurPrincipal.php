4f373426c63a5c4583ee715fcccaf9a1a639b9d6
truonganluc <truongan.luc@gmail.com>
2024-01-16 13:04:59 +0100

    "Page d'accueil"

--- controleur/controleurPrincipal.php
+++ controleur/controleurPrincipal.php
@@ -11,4 +11,3 @@ function controleurPrincipal($action){
     }
 }
 ?>
-}
--- vue/vueAccueil.php
+++ vue/vueAccueil.php
@@ -1,6 +1,5 @@
 <?php
     include "./include/header.html";
 ?>
-<div class="hero-text">
-    <h1>Truong-An LUC</h1>
+<div class="box">
 </div>
